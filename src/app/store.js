6f8b43510103d7058432dcd659588f17a5e4072e
keyur diwan <keyurdiwan009@gmail.com>
2022-01-14 13:35:50 +0530

    add redux for storing car data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import carReducer from '../features/Car/carSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    car: carReducer,
   },
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
-
+import { selectCars } from '../features/Car/carSlice';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
 
     const [burgerStatus, setBurgerStatus] = useState( false );
+    const cars = useSelector( selectCars );
 
     return (
         <Container>
@@ -15,10 +17,11 @@ const Header = () => {
             </a>
 
             <Menu>
-                <a href='#'>Model S</a>
-                <a href='#'>Model 3</a>
-                <a href='#'>Model X</a>
-                <a href='#'>Model Y</a>
+                {cars && cars.map( ( car, index ) =>(
+                     <a key = {index} href='#'> {car} </a>
+                ))}
+               
+                
             </Menu>
 
             <RightMenu>
@@ -32,12 +35,15 @@ const Header = () => {
                 <CloseWrapper>
                     <CustomClose onClick = { () => setBurgerStatus(false)} />
                 </CloseWrapper>
+                 {cars && cars.map( ( car, index ) =>(
+                     <li key = {index} > <a href = '#'> {car} </a> </li>
+                ))}
+               
                 
-                <li> <a href = '#'> Existing Inventory</a> </li>
                 <li> <a href = '#'> Used Inventory</a> </li>
                 <li> <a href = '#'> Trad-in </a> </li>
                 <li> <a href = '#'> Cybertruck</a> </li>
-                <li> <a href = '#'> Roadstar</a> </li>
+                <li> <a href = '#'> Roadstar</a > </li>
                 <li> <a href = '#'> Existing Inventory</a> </li>
             </BurgerNav>
         </Container>
